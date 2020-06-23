10376a74842692deba82fd9613c6407b918c83e1
showy <js.nb@qq.com>
2020-06-23 13:36:00 +0800

    update no-data-tip

---
+++ components/Common/NoDataTip.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="no-data-tip">
+    <img src="~/assets/images/no-data-tip.png" alt="/" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NoDataTip'
+}
+</script>
+
+<style lang="stylus" scoped>
+.no-data-tip {
+  position: relative;
+  height: 60rem;
+
+  img {
+    position: absolute;
+    top: 25%;
+    left: 50%;
+    width: 16rem;
+    transform: translateX(-50%);
+  }
+}
+</style>
